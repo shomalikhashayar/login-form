b848f49ef65d290bb381f6d4840c1134e8e8c7e8
Khashayar Shomali <khashi792017@gmail.com>
2023-01-11 23:29:16 +0330

    Make more beautiful

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@
 </template>
 
 <style>
+body {
+  background-color: rgb(224, 241, 255);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,7 +30,7 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #1aa3ff;
 }
 </style>
 <script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,19 @@
 <template>
-  <form @submit.prevent="submit">
-    <label for="">Email</label>
-    <input type="text" v-model="form.email" required />
-    <br /><br />
-    <label for="">password</label>
-    <input type="password" v-model="form.password" required />
-    <br /><br />
-    <button type="submit">Login</button>
-    <br />
-  </form>
+  <div class="container">
+    <form @submit.prevent="submit">
+      <label for="">Email</label>
+      <input type="text" v-model="form.email" required />
+      <br /><br />
+      <label for="">Password</label>
+      <input type="password" v-model="form.password" required />
+      <br /><br />
+      <div class="buttons">
+        <button type="submit">Login</button>
+        <button type="reset">Reset</button>
+      </div>
+      <br />
+    </form>
+  </div>
 </template>
 
 <script>
@@ -28,3 +33,77 @@ export default {
   },
 };
 </script>
+<style scoped>
+form {
+  background-color: #52baff;
+  width: 500px;
+  text-align: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  box-shadow: 0 10px 25px rgba(0, 0, 0, 0.3);
+  border-radius: 9px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+}
+.buttons{
+
+  margin-top: 20px;
+}
+.buttons > button {
+  padding-top: 15px;
+  padding-bottom: 15px;
+  padding-left: 25px;
+  padding-right: 25px;
+  margin: 5px;
+}
+
+button[type="submit"] {
+  background-color: rgb(16, 133, 57);
+  border-radius: 9px;
+  cursor: pointer;
+  font-weight: bold;
+  border: none;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  font-size: 14px;
+}
+button[type="submit"]:hover {
+  background-color: rgb(14, 91, 41);
+  color: white;
+  border-radius: 9px;
+  cursor: pointer;
+  font-weight: bold;
+}
+button[type="reset"] {
+  background-color: rgb(255, 160, 64);
+  border-radius: 9px;
+  cursor: pointer;
+  font-weight: bold;
+  border: none;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  font-size: 14px;
+}
+button[type="reset"]:hover {
+  background-color: rgb(255, 119, 0);
+  color: white;
+  border-radius: 9px;
+  cursor: pointer;
+  font-weight: bold;
+}
+input {
+  padding: 5px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  font-size: 14px;
+  font-weight: 600;
+  width: 300px;
+}
+label {
+  color: #ffffff;
+  font-weight: bold;
+  display: block;
+  margin-top: 20px;
+  margin-bottom: 5px;
+}
+label:after { content: ": " }
+</style>

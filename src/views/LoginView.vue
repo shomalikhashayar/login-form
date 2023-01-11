1e0655cdf13c79c8448331b7eb0e29fc6f204a41
Khashayar Shomali <khashi792017@gmail.com>
2023-01-11 22:56:26 +0330

    Every Thing is Fine :)

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form @submit.prevent="submit">
+    <label for="">Email</label>
+    <input type="text" v-model="form.email" required />
+    <br /><br />
+    <label for="">password</label>
+    <input type="password" v-model="form.password" required />
+    <br /><br />
+    <button type="submit">Login</button>
+    <br />
+  </form>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      this.$store.commit("login", this.form.email);
+    },
+  },
+};
 </script>
